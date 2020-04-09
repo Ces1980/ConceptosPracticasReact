45ed0450e1c0f81ed7bf734662a38cf2fbef19f1
CesarVE <thecesarvargas@gmail.com>
2020-04-09 18:20:58 -0500

    Primer componente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+/* function Hello (props){
+return <h2>{props.title}</h2>
+} */
+
+/* const Hello = (props) => <h2>{props.title}</h2> */
+
+class Hello extends Component{
+  render(){
+  return <h2>{this.props.title}</h2>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Hello title="Hola desde una prop"></Hello>
+        <div>
+            <h3>Vamos a aprender React</h3>
+            <p>Hola mundo estoy usando React</p>
+            <strong>Esto es un Strong</strong>
+        </div>
+      </header>{" "}
     </div>
   );
 }
