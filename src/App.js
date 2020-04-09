9f6893644168ac45c918f364b37d24bc7b5ffac0
CesarVE <thecesarvargas@gmail.com>
2020-04-09 18:55:15 -0500

    Entendiendo las props

--- src/App.js
+++ src/App.js
@@ -14,17 +14,37 @@ class Hello extends Component{
   }
 }
 
+class Text extends Component{
+   render(){
+     const textoSegunBool = this.props.isActivated ? 'Es cierto!' : 'Falso'
+     const mappedNumbers = this.props.arrayOfNumbers.map(n => n * 2)
+   return (
+     <div>
+          <p>{this.props.objectWithInfo.key}</p>
+          <p>{this.props.objectWithInfo.key2}</p>
+          <p>{this.props.text}</p>
+          <p>{this.props.number}</p>
+          <p>{textoSegunBool}</p>
+          <p>{this.props.arrayOfNumbers.join(', ')}</p>
+          <p>Arrelo manipulado antes de usarlo: {mappedNumbers.join(', ')}</p>
+     </div>
+  )
+   }
+  }
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
          <Hello title="Hola desde una prop"></Hello>
-        <div>
-            <h3>Vamos a aprender React</h3>
-            <p>Hola mundo estoy usando React</p>
-            <strong>Esto es un Strong</strong>
-        </div>
+         <Text 
+         arrayOfNumbers={[2,5,8]}
+         isActivated
+         objectWithInfo={{key: 'value', key2:'value2', key3:'value3'}}
+         number={2} 
+         text='Texto en String' 
+         ></Text>
       </header>{" "}
     </div>
   );
