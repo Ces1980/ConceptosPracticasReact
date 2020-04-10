ed7b8af7dfc190c0aa8d951ab950c73f41e5dc3e
CesarVE <thecesarvargas@gmail.com>
2020-04-09 19:55:03 -0500

    State: Gestión del estado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, {Component} from "react";
 
 
-class Title extends Component{
-  render(){
-  return <h1>{this.props.text}</h1>
+class Contador extends Component{
+  constructor(){
+    super()
+    this.state = {contador: 1}
   }
-}
-
-//Mensaje por default
-Title.defaultProps={
-  text:"Titulo por defecto"
+  render(){
+  return <span>{this.state.contador}</span>
+  }
 }
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        <Title text="Otro titulo desde las props"></Title>
+        <p>Primer componente con state</p>
+        <Contador></Contador>
       </div>
     )
   }
