b9fb239b83c191e222442291f3b0f2080ca67dfc
CesarVE <thecesarvargas@gmail.com>
2020-04-09 20:29:17 -0500

    State: Actualización del estado mediante setState()

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, {Component} from "react";
 
 
 class Contador extends Component{
- state = {contador: 0}
+constructor(){
+  super()
+  this.state = {contador: 1}
+  setInterval(() => {
+    this.setState({contador: this.state.contador +1})
+  },1000)
+}
   render(){
-  return(
-      <div>
-        <h2>El contador esta a {this.state.contador}</h2>
-      </div>
-  ) 
+  return <span>{this.state.contador}</span>
   }
 }
 
+/**Un componente se debe de tratar como información inmutable
+ * Se puede codificar mediante la función setState
+ * y es asincrono
+ */
+
 class App extends Component{
   render(){
-    return (
+    return(
       <div className="App">
-        <p>Primer componente con state</p>
-        <Contador></Contador>
+          <p>Primer componente con state</p>
+          <Contador></Contador>
       </div>
     )
   }
