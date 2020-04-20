e1a80e041b54e6d9a66c9cb0686f66a424437b30
CesarVE <thecesarvargas@gmail.com>
2020-04-20 11:41:05 -0500

    Eventos sinteticos

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, {Component} from "react";
 
 class App extends Component {
 
+  handleClick(e){
+console.log(e)
+console.log(e.nativeEvent)
+
+    alert('Hi here!')
+  }
   render(){
 
     return(
       <div className= "App">
         <h4>Eventos</h4>
-       <button onClick={()=> alert('Hi here!')}>Hi here!</button>
+       <button onClick={this.handleClick}>Hi here!</button>
       </div>
     );
   }
 }
 
 export default App;
+
+/* en React tenemos un sistema de eventos sintéticos que 
+usaremos como si fueran normales. Aunque están diseñados 
+para que pasen por eventos regulares, igual que JSX pasa
+por etiquetas HTML normales, cabe destacar que son una capa 
+que nos proporciona React y que no son eventos reales del DOM, 
+y por eso se llaman sintéticos. */
