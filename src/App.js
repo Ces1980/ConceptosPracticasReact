c0c956d5da3f749e7b637559ab969a4d462b5e05
CesarVE <thecesarvargas@gmail.com>
2020-04-20 12:18:25 -0500

    Eventos soportados: ejemplo con onMouseMove

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { mouseX: 0, mouseY: 0 }
+    //this.handleMouseMove = this.handleMouseMove.bind(this)
+  }
 
-  handleClick(e){
-console.log(e)
-console.log(e.nativeEvent)
+  handleMouseMove=(e) => {
+    const {clientX, clientY} = e
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
 
+  handleClick(e) {
+    console.log(e)
+    console.log(e.nativeEvent)
     alert('Hi here!')
   }
-  render(){
+  render() {
 
-    return(
-      <div className= "App">
+    return (
+      <div className="App">
         <h4>Eventos</h4>
-       <button onClick={this.handleClick}>Hi here!</button>
+        <button onClick={this.handleClick}>Hi here!</button>
+        <div
+        onMouseMove={this.handleMouseMove}
+          style={{ border: '1px solid red', marginTop: 10, padding: 10 }}>
+          <p>{this.state.mouseX},{this.state.mouseY}</p>
+        </div>
       </div>
     );
   }
@@ -21,9 +35,9 @@ console.log(e.nativeEvent)
 
 export default App;
 
-/* en React tenemos un sistema de eventos sintéticos que 
-usaremos como si fueran normales. Aunque están diseñados 
+/* en React tenemos un sistema de eventos sintéticos que
+usaremos como si fueran normales. Aunque están diseñados
 para que pasen por eventos regulares, igual que JSX pasa
-por etiquetas HTML normales, cabe destacar que son una capa 
-que nos proporciona React y que no son eventos reales del DOM, 
+por etiquetas HTML normales, cabe destacar que son una capa
+que nos proporciona React y que no son eventos reales del DOM,
 y por eso se llaman sintéticos. */
