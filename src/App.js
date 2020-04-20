5546328af838a895c60193c55440a12f6896743f
CesarVE <thecesarvargas@gmail.com>
2020-04-20 11:32:09 -0500

    El evento onClick

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React, {Component} from "react";
-import cars from './data/cars.json';
 
-class CarItem extends Component {
-  render(){
-    const { car } = this.props
-    return(
-      <li> 
-        <p><strong>Nombre: </strong>{car.name}</p>
-        <p><strong>Compañia: </strong>{car.company}</p>
-      </li>
-    )
-  }
-}
 class App extends Component {
 
   render(){
 
     return(
       <div className= "App">
-        <h4>Trabajando con listas de objetos</h4>
-        <ul>
-          {
-            cars.map( car=> {
-              return <CarItem key={car.id} car={car} />
-            })
-          }
-        </ul>
+        <h4>Eventos</h4>
+       <button onClick={()=> alert('Hi here!')}>Hi here!</button>
       </div>
     );
   }
