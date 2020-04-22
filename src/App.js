c0476ee0e659488ca9a848f5bf347ae50e0c7912
CesarVE <thecesarvargas@gmail.com>
2020-04-22 10:07:01 -0500

    La Prop especial Children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import Forms from './sections/forms'
+
+class Box extends Component {
+  render() {
+    return (
+      <div style={{ border: '1px solid #09f', margin: 5, padding: 5 }}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-       <Forms/>
+        <h3>Children props</h3>
+        <Box>Hola! Soy un children!</Box>
+        <Box>Otro Box con otro children</Box>
       </div>
     );
   }
