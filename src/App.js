d101d39faf29f2be79586ce6fba596c030498e04
CesarVE <thecesarvargas@gmail.com>
2020-04-22 10:18:12 -0500

    Uso de la  Prop Children

--- src/App.js
+++ src/App.js
@@ -10,14 +10,57 @@ class Box extends Component {
   }
 }
 
+class Article extends Component {
+  render() {
+    return (
+      <section>
+        <h2>{this.props.title}</h2>
+        <p><em>Escrito por {this.props.author}</em></p>
+        <Box>{this.props.date}</Box>
+        <article>
+          {this.props.children}
+        </article>
+      </section>
+    )
+  }
+}
+
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <h3>Children props</h3>
-        <Box>Hola! Soy un children!</Box>
-        <Box>Otro Box con otro children</Box>
+        <Article
+          author='César'
+          date={new Date().toLocaleDateString()}
+          title='Articulo sobre la prop children'
+        >
+          <p>El contenido lo envolvemos dentro del componente Article
+              será enviado al componente this.props.children.</p>
+          <strong>Y mantiene las etiquetas que se hayan añadido dentro</strong>
+        </Article>
+
+        <Article
+          author='César'
+          date={new Date().toLocaleDateString()}
+          title='Articulo 2'
+        >
+          <p>El contenido lo envolvemos dentro del componente Article
+              será enviado al componente this.props.children.</p>
+          <strong>Y mantiene las etiquetas que se hayan añadido dentro</strong>
+        </Article>
+
+        <Article
+          author='César'
+          date={new Date().toLocaleDateString()}
+          title='Otro Articulo'
+        >
+          <p>El contenido lo envolvemos dentro del componente Article
+              será enviado al componente this.props.children.</p>
+          <strong>Y mantiene las etiquetas que se hayan añadido dentro</strong>
+        </Article>
       </div>
     );
   }
