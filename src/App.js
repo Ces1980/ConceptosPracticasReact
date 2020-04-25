5f07a7b0e3621e8bb38766350842d15844120eda
CesarVE <thecesarvargas@gmail.com>
2020-04-25 16:44:35 -0500

    Método componentWillMount: una sola ejecución y se invoca antes de montar el componente y antes del render

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react'
+import ComponentWillMount from './sections/componentWillMount'
 
 
 
 class App extends Component {
 
-  /* Constructor por defecto
-  constructor(...args) {
-    super(...args)
-  } */
-
-  constructor(props) {
-    console.log('Constructor...')
-    super(props)//Este método llama al constructor de Component
-    //Inicializamos el state de nuestro componente
-    this.state = { mensajeInicial: 'mensaje inicial: ' }
-    // bineamos para enlazar el contexto correcto al método
-    this.handlerClick = this.handlerClick.bind(this)
-  }
-
-  /* Con arrow funtion tambien se puede enlazar al contexto correcto */
-  handlerClick() {
-    this.setState({ mensajeInicial: 'mensaje cambiado:  ' })
-  }
   render() {
-    console.log('render...')
     return (
       <div className="App">
-        <h3>Ciclo de montaje: constructor</h3>
-        {this.state.mensajeInicial}
-        <button onClick={this.handlerClick}>
-          Cambiar mensaje
-        </button>
+        <ComponentWillMount />
       </div>
     );
   }
